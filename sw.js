0f19bb3d23f54b9ba93277365087befa1e4cb940
Roberto Giacomazzi <roberto.giacomazzi@gmail.com>
2026-04-14 09:53:18 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Portafoglio Personale — Service Worker
-const CACHE_NAME = 'portafoglio-v7';
+const CACHE_NAME = 'portafoglio-v8';
 const CACHE_URLS = [
   '/PortafoglioPersonale/',
   '/PortafoglioPersonale/index.html',
